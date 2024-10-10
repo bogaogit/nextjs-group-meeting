aa88477e5bfdbda0ace1042deb21a671bf98bf8a
Bo Gao <bogaogit@gmail.com>
2024-10-10 17:55:06 +1000

    c

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export",
+  distDir: "out",
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
 };
